b372f30b3c2ea74c3cc6428d180aafe77f64c7bc
CoderXGC <xgc@tom.com>
2022-04-07 08:53:20 +0800

    里式替换原则

--- tutorials-2.3-1/src/test/java/com/ylesb/design/test/ApiTest.java
+++ tutorials-2.3-1/src/test/java/com/ylesb/design/test/ApiTest.java
@@ -8,13 +8,6 @@ import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 
-/**
- * @description: 测试
- * @author: 小傅哥，微信：fustack
- * @date: 2022/3/5
- * @github: https://github.com/fuzhengwei/CodeDesignTutorials
- * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
 public class  ApiTest {
 
     private Logger logger = LoggerFactory.getLogger(ApiTest.class);
