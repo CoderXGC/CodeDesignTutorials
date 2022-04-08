5e4ad797724ccafccfea172f2faa98a78799fc36
CoderXGC <xgc@tom.com>
2022-04-08 08:36:31 +0800

    迪米特原则（高内聚低耦合）

--- tutorials-2.4-0/src/main/java/com/ylesb/design/Student.java
+++ tutorials-2.4-0/src/main/java/com/ylesb/design/Student.java
@@ -1,12 +1,5 @@
-package cn.bugstack.design;
+package com.ylesb.design;
 
-/**
- * @description: 学生
- * @author: 小傅哥，微信：fustack
- * @date: 2022/3/8
- * @github: https://github.com/fuzhengwei/CodeDesignTutorials
- * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
 public class Student {
 
     private String name;    // 学生姓名
--- tutorials-2.4-0/src/main/java/com/ylesb/design/Teacher.java
+++ tutorials-2.4-0/src/main/java/com/ylesb/design/Teacher.java
@@ -1,15 +1,8 @@
-package cn.bugstack.design;
+package com.ylesb.design;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @description: 老师
- * @author: 小傅哥，微信：fustack
- * @date: 2022/3/8
- * @github: https://github.com/fuzhengwei/CodeDesignTutorials
- * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
 public class Teacher {
 
     private String name;                // 老师名称
