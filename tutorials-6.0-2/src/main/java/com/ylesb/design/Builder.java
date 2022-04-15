91c268d5391f7ba67536ce2ad3e27bde1f6f949e
CoderXGC <xgc@tom.com>
2022-04-15 16:44:09 +0800

    建造者模式

---
+++ tutorials-6.0-2/src/main/java/com/ylesb/design/Builder.java
@@ -0,0 +1,34 @@
+package com.ylesb.design;
+
+import com.ylesb.design.ceiling.LevelOneCeiling;
+import com.ylesb.design.ceiling.LevelTwoCeiling;
+import com.ylesb.design.coat.DuluxCoat;
+import com.ylesb.design.coat.LiBangCoat;
+import com.ylesb.design.floor.ShengXiangFloor;
+import com.ylesb.design.tile.DongPengTile;
+import com.ylesb.design.tile.MarcoPoloTile;
+
+public class Builder {
+
+    public IMenu levelOne(Double area) {
+        return new DecorationPackageMenu(area, "豪华欧式")
+                .appendCeiling(new LevelTwoCeiling())    // 吊顶，二级顶
+                .appendCoat(new DuluxCoat())             // 涂料，多乐士
+                .appendFloor(new ShengXiangFloor());     // 地板，圣象
+    }
+
+    public IMenu levelTwo(Double area){
+        return new DecorationPackageMenu(area, "轻奢田园")
+                .appendCeiling(new LevelTwoCeiling())   // 吊顶，二级顶
+                .appendCoat(new LiBangCoat())           // 涂料，立邦
+                .appendTile(new MarcoPoloTile());       // 地砖，马可波罗
+    }
+
+    public IMenu levelThree(Double area){
+        return new DecorationPackageMenu(area, "现代简约")
+                .appendCeiling(new LevelOneCeiling())   // 吊顶，一级顶
+                .appendCoat(new LiBangCoat())           // 涂料，立邦
+                .appendTile(new DongPengTile());        // 地砖，东鹏
+    }
+
+}
